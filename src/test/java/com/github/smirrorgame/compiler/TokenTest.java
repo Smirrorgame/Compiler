9c056928625e72af45fd71b3753d141ccaacfd9c
Smirrorgame <micha.halla@student.uni-luebeck.de>
2022-08-20 18:06:24 +0200

    App.java: 	- printing out the generated Tokens
    Lexer.java:
    	- implemented tokenization of single character Tokens
    	- implemented tokenization of one-or-more character Tokens
    	- implemented tokenization of strings
    	- implemented tokenization of numbers (all being interpreted as double)
    	- implemented tokenization of reserved keywords
    
    Token.java:
    	- implemented toString() and equals
    
    LexerTest.java:
    	- implemented several Unit-tests for Lexer.java
    
    TokenTest.java:
    	- implemented Unit-tests fotr equals and toString()

--- src/test/java/com/github/smirrorgame/compiler/TokenTest.java
+++ src/test/java/com/github/smirrorgame/compiler/TokenTest.java
@@ -1,6 +1,7 @@
 package com.github.smirrorgame.compiler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,34 @@ public class TokenTest {
 		assertEquals(0, t.line);
 		// TODO: add far more test cases
 		System.out.println("Needs to be implemented further!");
+	}
+	
+	@Test
+	public void test_toString() {
+		Token t = new Token(TokenType.AND, "and", null, 0);
+		assertEquals("AND: and null", t.toString());
+	}
+	
+	@Test
+	public void test_equals() {
+		Token expected = new Token(TokenType.STRING, "\"the string\"", "the string", 1);
+		Token actual = new Token(TokenType.STRING, "\"the string\"", "the string", 1);
+		Token unexpected = new Token(TokenType.STRING, "\"the string\"", "the string", 1);
+		assertEquals(expected, actual);
+		actual = new Token(TokenType.STRING, "\the string\"", null, 1);
+		assertNotEquals(unexpected, actual);
+		assertNotEquals(actual, unexpected);
+		
+		unexpected = new Token(TokenType.LET, "let", null, 1);
+		actual = new Token(TokenType.STRING, "\the string\"", "the string", 1);
+		assertNotEquals(unexpected, actual);
+		assertNotEquals(actual, unexpected);
+
+		expected = new Token(TokenType.LET, "let", null, 1);
+		actual = new Token(TokenType.LET, "let", null, 1);
+		assertEquals(expected, actual);
+		
+		assertNotEquals(actual, TokenType.LET);
 	}
 	
 	
