b1f8f56958103dc264a7f6c14a12e1a2ee1bcbaf
Smirrorgame <micha.halla@student.uni-luebeck.de>
2022-08-23 13:16:17 +0200

    added visitor pattern and nested Expr-subclasses inside Expr class

--- src/main/java/com/github/smirrorgame/compiler/Expr.java
+++ src/main/java/com/github/smirrorgame/compiler/Expr.java
@@ -1,42 +1,78 @@
 package com.github.smirrorgame.compiler;
 
 abstract class Expr {
-}
-
-class Binary extends Expr {
-	Binary(Expr left, Token operator, Expr right) {
-		this.left = left;
-		this.operator = operator;
-		this.right = right;
+	interface Visitor<T> {
+		T visitBinaryExpr(Binary binary);
+		T visitGroupingExpr(Grouping grouping);
+		T visitLiteralExpr(Literal literal);
+		T visitUnaryExpr(Unary unary);
 	}
 
-	final Expr left;
-	final Token operator;
-	final Expr right;
-}
+	static class Binary extends Expr {
 
-class Grouping extends Expr {
-	Grouping(Expr expression) {
-		this.expression = expression;
+		final Expr left;
+		final Token operator;
+		final Expr right;
+
+		Binary(Expr left, Token operator, Expr right) {
+			this.left = left;
+			this.operator = operator;
+			this.right = right;
+		}
+
+		@Override
+		<T> T accept(Visitor<T> visitor) {
+			return visitor.visitBinaryExpr(this);
+		}
+
 	}
 
-	final Expr expression;
-}
+	static class Grouping extends Expr {
 
-class Literal extends Expr {
-	Literal(Object value) {
-		this.value = value;
+		final Expr expression;
+
+		Grouping(Expr expression) {
+			this.expression = expression;
+		}
+
+		@Override
+		<T> T accept(Visitor<T> visitor) {
+			return visitor.visitGroupingExpr(this);
+		}
+
 	}
 
-	final Object value;
-}
+	static class Literal extends Expr {
 
-class Unary extends Expr {
-	Unary(Token operator, Expr right) {
-		this.operator = operator;
-		this.right = right;
+		final Object value;
+
+		Literal(Object value) {
+			this.value = value;
+		}
+
+		@Override
+		<T> T accept(Visitor<T> visitor) {
+			return visitor.visitLiteralExpr(this);
+		}
+
 	}
 
-	final Token operator;
-	final Expr right;
+	static class Unary extends Expr {
+
+		final Token operator;
+		final Expr right;
+
+		Unary(Token operator, Expr right) {
+			this.operator = operator;
+			this.right = right;
+		}
+
+		@Override
+		<T> T accept(Visitor<T> visitor) {
+			return visitor.visitUnaryExpr(this);
+		}
+	}
+	
+	abstract <T> T accept(Visitor<T> visitor);
+	
 }
